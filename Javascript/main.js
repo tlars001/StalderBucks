0ed9b01432271c2321c35956fcfea0b65cdda797
Trevor Larson <trevor.larson2011@gmail.com>
2023-09-03 12:52:57 -0600

    Initial Test

---
+++ Javascript/main.js
@@ -0,0 +1,35 @@
+//======================================================================================================================
+function login()
+{
+}
+
+//======================================================================================================================
+function createAccount()
+{
+}
+
+//======================================================================================================================
+function populateLoginAccounts()
+{
+    var accountList = document.getElementById("accountOptions");
+    var accounts = readAccountsFile();
+
+    accounts.forEach(element => {
+        accountList.appendChild(element);
+    });
+}
+
+//======================================================================================================================
+function readAccountsFile()
+{
+    var accounts;
+
+    fetch("./Resources/Accounts.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    accounts = text.split("\n");
+   })
+  .catch((e) => console.error(e));
+
+   return accounts;
+}
